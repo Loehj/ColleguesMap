45b427935b5c7f56762e4c00eb3601fbffca55af
LoehJ <johannes.loehj@gmail.com>
2022-02-06 15:32:53 +0100

    Switched to sqlite

--- server.js
+++ server.js
@@ -1,41 +1,84 @@
 const express = require('express');
-const { Client } = require('pg');
-
-// load env variables from .env for development
-require('dotenv').config();
-
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL || '');
-
-const client = new Client({
-	user: config.user,
-	host: config.host,
-	database: config.database,
-	password: config.password,
-	port: config.port,
-    ssl: true
-});
-
+const { get } = require('express/lib/request');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`Running on port ${port}`));
-client.connect();
-app.get('/', async (req, res) => {
 
-	try {
-		const result = await client.query('SELECT NOW()');
-
-		// TODO: Do something with the result
-	} catch (e) {
-		console.error('Error while fetching query', e);
-		res.sendStatus(500);
-        return;
-	}
-
-	res.status(200).send('Test');
+app.get('/getCities', async (req, res) => {
+	var db = new sqlite3.Database('./db/citiesDB.db', (err) => {
+		if (err) {
+    		console.error(err.message);
+		}
+	})
+	db.all(`SELECT * FROM cities`, (err, rows) => {
+		if (err){
+		  res.status(500).send(err);
+		}
+		res.status(200).send(rows);
+	});
+	db.close((err) => {
+		if (err) {
+			console.error(err.message);
+		}
+	})
 });
 
 app.post('/addCity', (req, res) => {
 	console.log(req.query);
-	res.status(200).send('hallo API');
+	if(req.query.city != null && req.query.lat != null && req.query.lng != null && req.query.city != "" && req.query.lat != "" && req.query.lng != ""){
+		var db = new sqlite3.Database('./db/citiesDB.db', (err) => {
+			if (err) {
+				console.error(err.message);
+			}
+		})
+		db.run(`INSERT INTO cities(name, lat, lng)
+			VALUES('${req.query.city}', '${req.query.lat}', '${req.query.lng}')
+		`);
+		res.status(200).send(`${req.query.city} added`);
+	} else {
+		res.status(500).send(`invalid input: ${req.query}`);
+	}
+	db.close((err) => {
+		if (err) {
+			console.error(err.message);
+		}
+	})
 });
+
+app.get('/getCitiesGrouped', async (req, res) => {
+	var db = new sqlite3.Database('./db/citiesDB.db', (err) => {
+		if (err) {
+    		console.error(err.message);
+		}
+	})
+	db.all(`SELECT name, lat, lng, COUNT(*) as count FROM cities GROUP BY name, lat, lng`, (err, rows) => {
+		if (err){
+		  res.status(500).send(err);
+		}
+		res.status(200).send(rows);
+	});
+	db.close((err) => {
+		if (err) {
+			console.error(err.message);
+		}
+	})
+});
+
+/* Clear TABLE
+var db = new sqlite3.Database('./db/citiesDB.db', (err) => {
+	if (err) {
+		console.error(err.message);
+	}
+})
+db.serialize(() => {
+	db.run(`DELETE FROM cities`)
+	.run(`VACUUM`);
+});
+db.close((err) => {
+	if (err) {
+		console.error(err.message);
+	}
+})
+*/
